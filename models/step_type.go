bc0986e1007df10064cadc053d78719191494c1f
Raz <rregev@redhat.com>
2020-06-29 19:45:55 +0300

    MGMT-1205 Autogenerated swagger code (reset cmd)

--- models/step_type.go
+++ models/step_type.go
@@ -37,6 +37,9 @@ const (
 
 	// StepTypeFreeNetworkAddresses captures enum value "free-network-addresses"
 	StepTypeFreeNetworkAddresses StepType = "free-network-addresses"
+
+	// StepTypeReset captures enum value "reset"
+	StepTypeReset StepType = "reset"
 )
 
 // for schema
@@ -44,7 +47,7 @@ var stepTypeEnum []interface{}
 
 func init() {
 	var res []StepType
-	if err := json.Unmarshal([]byte(`["hardware-info","connectivity-check","execute","inventory","install","free-network-addresses"]`), &res); err != nil {
+	if err := json.Unmarshal([]byte(`["hardware-info","connectivity-check","execute","inventory","install","free-network-addresses","reset"]`), &res); err != nil {
 		panic(err)
 	}
 	for _, v := range res {
